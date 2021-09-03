ed54821ea8a7b6f6da7344a82b19e4c8d8cb1f6e
cardstotheheart@gmail.com <36455793+domisjustanumber@users.noreply.github.com>
2021-09-03 16:16:58 -0400

    Added an MPLABX Project

---
+++ MPLABX/Arc Lighter.X/build/default/production/_ext/1852409773/main.p1.d
@@ -0,0 +1,2 @@
+build/default/production/_ext/1852409773/main.p1:  \
+../../Code/main.c 
---
+++ MPLABX/Arc Lighter.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Arc_Lighter.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Arc_Lighter.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=arclighter.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/arclighter.x.tar
